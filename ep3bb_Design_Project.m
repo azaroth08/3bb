164c6571c88a8ec5f33f00662d3221418973ea03
azaroth08 <37883024+azaroth08@users.noreply.github.com>
2018-03-31 00:30:11 -0400

    Add files via upload

---
+++ ep3bb_Design_Project.m
@@ -0,0 +1,215 @@
+function varargout = ep3bb_Design_Project(varargin)
+
+
+% Begin initialization code - DO NOT EDIT
+clc; 
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ep3bb_Design_Project_OpeningFcn, ...
+                   'gui_OutputFcn',  @ep3bb_Design_Project_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+
+% --- Executes just before ep3bb_Design_Project is made visible.
+function ep3bb_Design_Project_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to ep3bb_Design_Project (see VARARGIN)
+
+% Choose default command line output for ep3bb_Design_Project
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes ep3bb_Design_Project wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = ep3bb_Design_Project_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% --- Executes on button press in start.
+function start_Callback(hObject, eventdata, handles)
+global run
+global set
+setpoint = 25;
+run=1;
+i=2;
+global proportional_term
+global integral_term
+global derivative_term
+proportional_term = 0.1; %initialize the PID values
+integral_term = 0;
+derivative_term = 0;
+text = fopen('new.txt','a');
+fmt = '%5d \n'; %test text file for diagnostic purposes
+%Determine the optimal values for the proportional, integral and derivative 
+%terms via experimentation 
+old_pos = 280; %original position of the motor
+comport = serial('COM3', 'baud', 155300, 'FlowControl','none');
+fopen(comport);
+fwrite(comport, old_pos/3);
+while (run==1)
+   clock_input = fread(comport,1,'uint16');
+
+   x(i) = ((clock_input)/2000000*34300/2); %current position of the ball 
+   err(i) = x(i)-setpoint; %calculates error from position
+   if (x==2) %initializes x so derivative term stays within bounds
+       x(1) = x(2);
+       err(1) = err(2);
+   end
+   P_out = proportional_term*err(i);
+   I_out = integral_term*trapz(err); %PID term calculation
+   D_out = derivative_term*(x(i)-x(i-1));
+   
+   plot(2:i,err(2:i));
+   ylim([-25 25]);
+   new_pos = old_pos +P_out+I_out +D_out; %new position of ball 
+   fprintf(text,fmt,new_pos); %diagnostics text file write
+   %compares the new_pos to the limits and changes it to match the absolute
+   %limits. limits values may need to be updated with number of steps. 
+   i=i+1;   
+   if (new_pos > 300)
+       new_pos = 300;
+   elseif (new_pos <230)
+       new_pos = 230;
+   end
+   old_pos = new_pos; 
+   fwrite(comport, new_pos/3);
+   drawnow
+end
+%plot(i,x)
+fclose(comport);
+fclose(text);
+
+% --- Executes on button press in stop.
+function stop_Callback(hObject, eventdata, handles)
+global run;
+run=0;
+
+
+% --- Executes on button press in quit.
+function quit_Callback(hObject, eventdata, handles)
+global run
+run=0;
+fclose(instrfind);
+close all;
+
+
+
+function middle_Callback(hObject, eventdata, handles)
+% hObject    handle to middle (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of middle as text
+%        str2double(get(hObject,'String')) returns contents of middle as a
+%        double
+global set
+set=str2double(get(hObject,'String'));
+
+% --- Executes during object creation, after setting all properties.
+function middle_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to middle (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function proportional_Callback(hObject, eventdata, handles)
+% hObject    handle to middle (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of middle as text
+%        str2double(get(hObject,'String')) returns contents of middle as a
+%        double
+global proportional_term
+proportional_term=str2double(get(hObject,'String'));
+
+% --- Executes during object creation, after setting all properties.
+function proportional_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to proportional (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function integral_Callback(hObject, eventdata, handles)
+% hObject    handle to integral (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of integral as text
+%        str2double(get(hObject,'String')) returns contents of integral as a double
+global integral_term
+integral_term=str2double(get(hObject,'String'));
+
+% --- Executes during object creation, after setting all properties.
+function integral_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to integral (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function derivative_Callback(hObject, eventdata, handles)
+% hObject    handle to derivative (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of derivative as text
+%        str2double(get(hObject,'String')) returns contents of derivative as a double
+global derivative_term
+derivative_term=str2double(get(hObject,'String'));
+
+% --- Executes during object creation, after setting all properties.
+function derivative_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to derivative (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
---
+++ main.c
@@ -0,0 +1,140 @@
+
+#include "io430.h"
+int a,b,c,d, new_pos =280,pos=0;
+void find_dist();
+
+void putc(unsigned char c)
+{
+  while (!IFG2_bit.UCA0TXIFG);{
+  UCA0TXBUF = c;}
+}
+
+unsigned char getc(void)
+{
+ while (!IFG2_bit.UCA0RXIFG);
+return (UCA0RXBUF);
+}
+
+void main( void )
+{
+  // Stop watchdog timer to prevent time out reset
+   WDTCTL = WDTPW + WDTHOLD;
+  BCSCTL1 = CALBC1_16MHZ;
+  DCOCTL = CALDCO_16MHZ;
+  P2DIR=0xFF;
+  //P2DIR = 0;
+  P1DIR_bit.P0 = 1;
+  P1OUT_bit.P0 = 0;
+  __enable_interrupt();
+  //initialize timer 1
+  TA1CTL_bit.TASSEL1 = 1;
+  TA1CTL_bit.TASSEL0 = 0;
+  TA1CTL_bit.MC1 = 0;
+  TA1CTL_bit.MC0 = 1;
+  TA1CTL_bit.ID1 = 1;
+  TA1CTL_bit.ID0 = 0;
+  TA1CCTL0=CCIE;
+  TA1CCR0=20000;
+  // Initialize timer 0
+  TA0CTL_bit.TASSEL1 = 1;
+  TA0CTL_bit.TASSEL0 = 0;
+  TA0CTL_bit.MC1 = 1;
+  TA0CTL_bit.MC0 = 0;
+  TA0CTL_bit.ID1 = 1;
+  TA0CTL_bit.ID0 = 1;
+  P2OUT =1;
+  P1SEL |= BIT1 + BIT2;
+  P1SEL2 |=BIT1+BIT2;
+  UCA0CTL1 |=UCSSEL1;
+  UCA0BR1 = 0;
+  UCA0BR0 = 103;
+  UCA0CTL1 &= ~UCSWRST;
+  
+  while(1){
+    new_pos = getc()*3;
+    find_dist();
+    __delay_cycles(300000);
+    
+  }
+}
+/*
+#pragma vector=PORT1_VECTOR
+__interrupt void bit3(void) { 
+  int buffer;
+  TA0R = 0;
+  while(P1IN_bit.P3){
+  buffer = TA0R;
+  }
+  putc(buffer);
+  putc(buffer >>8);
+  P1IFG_bit.P3 = 0;
+}
+*/
+
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void timer2(void) {
+  if (pos > new_pos){
+    a = 1; b = 2; c = 4; d = 8;
+
+   if(P2OUT==a)
+      P2OUT =a+b;
+   else if (P2OUT == a+b)
+     P2OUT = b;
+   else if (P2OUT == b)
+     P2OUT = b+c;
+   else if (P2OUT == b+c)
+     P2OUT = c;
+   else if (P2OUT == c)
+     P2OUT = c+d;
+   else if (P2OUT==c+d)
+     P2OUT =d;
+   else if (P2OUT== d)
+     P2OUT= d+a;
+   else if (P2OUT == d+a)
+     P2OUT = a;
+   pos--;
+  }
+   else if (pos < new_pos){
+    a = 8; b = 4; c = 2; d = 1;
+
+   if(P2OUT==a)
+      P2OUT =a+b;
+   else if (P2OUT == a+b)
+     P2OUT = b;
+   else if (P2OUT == b)
+     P2OUT = b+c;
+   else if (P2OUT == b+c)
+     P2OUT = c;
+   else if (P2OUT == c)
+     P2OUT = c+d;
+   else if (P2OUT==c+d)
+     P2OUT =d;
+   else if (P2OUT== d)
+     P2OUT= d+a;
+   else if (P2OUT == d+a)
+     P2OUT = a;
+   pos++;
+  }
+   else {}
+  TA1R=0;
+}
+
+
+void find_dist() {
+  unsigned int buffer, i=0; 
+  P1OUT_bit.P0 = 1;
+  __delay_cycles(20);
+  P1OUT_bit.P0 = 0;
+  while(!P1IN_bit.P3) {
+    TA0R = 0;
+    i++;
+    if (i>60000){
+      break;
+    }
+  }
+  while(P1IN_bit.P3) {
+    buffer = TA0R;
+  }
+  putc(buffer);
+  putc(buffer >>8);
+}
